b1d10e3350b133474cf45114cb5cf876d455da25
Fabricio <fctofani2@gmail.com>
2019-11-28 11:36:11 -0300

    Solução inicial

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int** leGrafo(int&);
 void imprimeMatriz(int**, int);
 short deletaMatriz(int**, int);
+int solucaoInicial(int**, int);
 
 int main()
 {
     int tam;
     int** mat = leGrafo(tam);
-//    imprimeMatriz(mat, tam);
+    int* solucao = new int[tam];
+    int otima = solucaoInicial(mat, tam);
+    cout << otima << endl;
+    imprimeMatriz(mat, tam);
     deletaMatriz(mat, tam);
     return 0;
 }
@@ -27,6 +31,33 @@ int** leGrafo(int& tamanho){
     }
     fclose(arquivo);
     return matriz;
+}
+
+int solucaoInicial(int** grafo, int tam){
+    int* solucao = new int[tam];
+    for(int i = 0; i < tam; i++) solucao[i] = 0;
+
+    solucao[0] = 1; // coloca o primeiro vértice na solução
+    bool flag = true; // checa se o vértice atual é adjacente ao que está sendo comparado
+    int cont = 1; // qtd de vértices na solução
+
+    for(int i = 1; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            if(grafo[i][j] == 1 && solucao[j] == 1){
+                j = tam;
+                flag = false;
+            }
+        }
+        if(flag){
+            solucao[i] = 1;
+            cont++;
+        }
+        flag = true;
+    }
+    for(int i = 0; i < tam; i++) cout << " | " << solucao[i];
+    cout << endl;
+
+    return cont;
 }
 
 void imprimeMatriz(int** matriz, int tamanho){
